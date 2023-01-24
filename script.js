abbaf6581597d79011806ba3de269842d46c0b30
Chris <chriscablish@hotmail.com>
2023-01-24 14:51:32 -0600

    update score selection on thank you page after submit

--- script.js
+++ script.js
@@ -4,10 +4,38 @@ const numberThreeEl = document.getElementById('number-three');
 const numberFourEl = document.getElementById('number-four');
 const numberFiveEl = document.getElementById('number-five');
 
+let rating 
+
+numberOneEl.addEventListener("click", ()=>{
+    rating = 1
+    console.log(rating)
+})
+numberTwoEl.addEventListener("click", ()=>{
+    rating = 2
+    console.log(rating)
+})
+numberThreeEl.addEventListener("click", ()=>{
+    rating = 3
+    console.log(rating)
+})
+numberFourEl.addEventListener("click", ()=>{
+    rating = 4
+    console.log(rating)
+})
+numberFiveEl.addEventListener("click", ()=>{
+    rating = 5
+    console.log(rating)
+})
+
+const selection = document.getElementById('number');
+
+
+
+
+
 
 const submitEl = document.getElementById('submit-btn');
 
-
 submitEl.addEventListener("click", ()=> {
-    console.log('please work');
+    selection.innerText = `${rating}`;
 })
