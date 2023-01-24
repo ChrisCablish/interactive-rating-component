b7aa3e1f478248203da9fba7b94fc7def9b728c1
Chris <chriscablish@hotmail.com>
2023-01-24 14:33:09 -0600

    add javascript variables and event listener

---
+++ script.js
@@ -0,0 +1,13 @@
+const numberOneEl = document.getElementById('number-one');
+const numberTwoEl = document.getElementById('number-two');
+const numberThreeEl = document.getElementById('number-three');
+const numberFourEl = document.getElementById('number-four');
+const numberFiveEl = document.getElementById('number-five');
+
+
+const submitEl = document.getElementById('submit-btn');
+
+
+submitEl.addEventListener("click", ()=> {
+    console.log('please work');
+})
